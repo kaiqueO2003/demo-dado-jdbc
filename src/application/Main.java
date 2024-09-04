5be800d7f947af72615232ad1cabfb27b1f2eee5
kaique oliveira <kaique20003@gmail.com>
2024-09-04 11:08:02 -0300

    Reusing insantition

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,11 +10,10 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-        Seller seller = new Seller(21, "bob","bob@gmail.com", new Date(), 3000.0, obj);
-        System.out.println(obj);
-        System.out.println(seller);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println("--- TEST 1: sellerfindById");
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
     }
 }
